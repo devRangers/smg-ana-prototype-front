12b705f001d308353703f46a8085652e2a21edbb
rnrn99 <giu0213@naver.com>
2022-05-29 01:56:24 +0900

    [Update] list table

--- src/components/CameraComp.js
+++ src/components/CameraComp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AiFillCamera } from "react-icons/ai";
 import axios from "axios";
 
@@ -6,6 +6,7 @@ function CameraComp() {
   const [file, setFile] = useState(null);
   const [source, setSource] = useState(null);
   const [result, setResult] = useState(null);
+  const [list, setList] = useState([]);
 
   const handleCapture = (target) => {
     if (target.files) {
@@ -27,9 +28,18 @@ function CameraComp() {
         "Content-Type": "multipart/form-data",
       },
     });
-    console.log(res.data);
     setResult(res.data.species);
   };
+
+  useEffect(() => {
+    if (result) {
+      async function getList() {
+        const res = await axios.get(`https://ana-prototype-back.herokuapp.com/photos/${result}`);
+        setList(res.data);
+      }
+      getList();
+    }
+  }, [result]);
 
   return (
     <div>
@@ -71,7 +81,29 @@ function CameraComp() {
       >
         결과 가져오기
       </button>
+
       {result && <h1>종 : {result}</h1>}
+
+      {list.length > 0 && (
+        <table border="1">
+          <th>종</th>
+          <th>이름</th>
+          <th>발견 위치</th>
+          <th>특징</th>
+          <th>사진</th>
+          {list.map((item) => (
+            <tr>
+              <td>{item.species}</td>
+              <td>{item.name}</td>
+              <td>{item.location}</td>
+              <td>{item.feature}</td>
+              <td>
+                <img src={item.imageURL} alt="animal" width="200" />
+              </td>
+            </tr>
+          ))}
+        </table>
+      )}
     </div>
   );
 }
